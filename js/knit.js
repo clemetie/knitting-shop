c80a7fe6437210bb73414718ead77ba60099f15c
hyobeen <a01035083619@gmail.com>
2025-01-22 17:28:04 +0900

    no message

--- js/knit.js
+++ js/knit.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   // 햄버거바 누르면 메뉴가 슝 ..
+
   $("#hamburger-menu").click(function () {
     $(".mobile-nav").animate(
       {
@@ -12,9 +13,81 @@ $(document).ready(function () {
   $(".close").click(function () {
     $(".mobile-nav").animate(
       {
-        right: -200,
+        right: -300,
       },
       300
     );
   });
+  $("#hamburger-menu").on("click", function () {
+    $(".mobile-wrap").addClass("active");
+  });
+  $(".close").on("click", function () {
+    $(".mobile-wrap").removeClass("active");
+  });
+
+  // 모달 닫기 (배경 클릭)
+  $(".mobile-wrap").on("click", function (e) {
+    if ($(e.target).is(".mobile-wrap")) {
+      $(".mobile-wrap").removeClass("active");
+    }
+  });
 });
+window.onload = function(){
+  var swiper = new Swiper(".bestSwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    Loop: true,
+    grid: {
+      rows: 3,
+      fill: "row",
+    },
+    breakpoints: {
+      // centeredSlides: true,
+
+      480: {
+        slidesPerView: 3,
+        grid: {
+          rows: 2, // 행: 2개
+        },
+       
+      },
+
+      768: {
+        slidesPerView: 3.5,
+        grid: {
+          rows: 1, // 행: 2개
+        },
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  var swiper = new Swiper(".visualSwiper", {
+    centeredSlides: true,
+    roop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  var swiper = new Swiper(".mobileSwiper", {
+    centeredSlides: true,
+    Loop: true,
+    rewind: true,
+    spaceBetween: 20,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
